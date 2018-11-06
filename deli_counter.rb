a97e0821f2e495d1c2b8df9247b48d37360a8552
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 20:36:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.size > 0
+    current_line = "The line is currently:"
+    array.each.with_index(1) do |person, i|
+    current_line << " #{i}. #{person}"
+   end
+     puts current_line
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}." 
+    array.shift
+  end
+end
+#Build the now_serving method which should call out (i.e. puts) the next person in line and
+#then remove them from the front. If there is nobody in line, it should call out (puts)
+#that "There is nobody waiting to be served!".
